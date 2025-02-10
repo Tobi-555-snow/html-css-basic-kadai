2aa5799f1ddcef00b51da74452714abcf845bd7c
Tobi-555-snow <akikotobi@hotmail.co.jp>
2025-02-10 12:55:03 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const Today = new Date();
-
-const Year = Today.getFullYear();
-const Month = Today.getMonth();
-const Day = Today.getDate();
-
-
-console.log(
- Year + '年' + (Month + 1) + '月' + Day + '日');
